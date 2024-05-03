2fdc899648f270f3c7ef05929e1c5561514816ec
Artem Hryshko <artemggg2222@gmail.com>
2024-05-03 17:54:47 +0300

    add logic for spider(analiz vacancies)

--- get_technologies_data/spiders/technologies.py
+++ get_technologies_data/spiders/technologies.py
@@ -1,10 +1,35 @@
+from typing import Generator
+
 import scrapy
+from scrapy.http import Response
 
 
 class TechnologiesSpider(scrapy.Spider):
     name = "technologies"
-    allowed_domains = ["djinni.co"]
-    start_urls = ["https://djinni.co/jobs/?primary_keyword=Python"]
+    allowed_domains = ["www.work.ua"]
+    start_urls = ["https://www.work.ua/jobs-python+developer/"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: Response, **kwargs) -> Generator:
+        job_vacancies = response.css(".cut-top.cut-bottom a::attr(href)").getall()
+        for vacancy in job_vacancies:
+            yield scrapy.Request(
+                url=response.urljoin(vacancy), callback=self._parse_single_book
+            )
+
+        next_page = response.css(
+            "#pjax-job-list > nav > "
+            "ul.pagination.text-center.hidden-xs > "
+            "li.no-style.add-left-default > a::attr(href)"
+        ).get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def _parse_single_book(self, response: Response) -> Generator:
+        technologies = response.css(
+            ".flex.flex-wrap.w-100.toggle-block."
+            "overflow.block-relative.js-toggle-block > "
+            "span > span::text"
+        ).getall()
+        yield {
+            "technologies": technologies,
+        }
